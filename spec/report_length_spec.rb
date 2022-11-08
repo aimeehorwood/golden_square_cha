d6f5df653fff5365631d835acd8fdd9668468891
Aimee <aimeehorwood@hotmail.com>
2022-11-08 15:52:59 +0000

    Report length

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -8,11 +8,11 @@ RSpec.describe "report length method" do
     result = report_length("Aimee")
     expect(result).to eq "This string was 5 characters long."
   end
-  it "returns a given strign length including white space" do
+  it "returns a given strings length including white space" do
     result = report_length("Hello World")
     expect(result).to eq "This string was 11 characters long."
   end
-  it "returns a given strign length including special characters and white space" do
+  it "returns a given strings length including special characters and white space" do
     result = report_length("h£ll*   W@rlD")
     expect(result).to eq "This string was 13 characters long."
   end
